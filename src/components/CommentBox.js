7149a79d273edaa6f93fe3400fb3f15f9eae68e3
Magesh <magesh@hash14.com>
2018-12-19 18:22:15 +0530

    Higher order component created

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions";
+import requireAuth from "../components/require_auth";
 
 class CommentBox extends Component {
   state = {
@@ -42,4 +43,4 @@ class CommentBox extends Component {
 export default connect(
   null,
   actions
-)(CommentBox);
+)(requireAuth(CommentBox));
